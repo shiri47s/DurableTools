4e6581039fcc90b5f2fae2bb39b5498ff385508c
shima <sh1m@outlook.jp>
2024-04-22 15:22:06 +0900

    Update versions for 1.20.4.

--- common/src/main/java/com/shiri47s/mod/durabletools/blocks/DurableTorchBlock.java
+++ common/src/main/java/com/shiri47s/mod/durabletools/blocks/DurableTorchBlock.java
@@ -1,19 +1,15 @@
 package com.shiri47s.mod.durabletools.blocks;
 
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.block.TorchBlock;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
 
 public class DurableTorchBlock extends TorchBlock {
     public DurableTorchBlock() {
         super(
+                ParticleTypes.FLAME,
                 AbstractBlock
                         .Settings
                         .create()
@@ -21,12 +17,6 @@ public class DurableTorchBlock extends TorchBlock {
                         .breakInstantly()
                         .luminance(arg -> 14)
                         .sounds(BlockSoundGroup.WOOD)
-                        .pistonBehavior(PistonBehavior.DESTROY)
-                , ParticleTypes.FLAME);
-    }
-
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return BOUNDING_SHAPE;
+                        .pistonBehavior(PistonBehavior.DESTROY));
     }
 }
