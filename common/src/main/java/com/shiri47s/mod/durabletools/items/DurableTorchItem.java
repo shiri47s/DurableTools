7fb0e349eadbca93298442ff82ed7423329b0850
shima <sh1m@outlook.jp>
2024-04-26 15:43:01 +0900

    Modified archive name.

--- common/src/main/java/com/shiri47s/mod/durabletools/items/DurableTorchItem.java
+++ common/src/main/java/com/shiri47s/mod/durabletools/items/DurableTorchItem.java
@@ -28,11 +28,11 @@ public class DurableTorchItem extends DurableToolsItem {
     private static final int USE_COST = 2;
 
     public DurableTorchItem(Settings settings) {
-        this(DurableTorchMaterial.INSTANCE, settings);
+        this(DurableTorchMaterial.INSTANCE, ATTACK_DAMAGE, ATTACK_SPEED, settings);
     }
 
-    public DurableTorchItem(ToolMaterial material, Settings settings) {
-        super(material, ATTACK_DAMAGE, ATTACK_SPEED, settings);
+    public DurableTorchItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
     }
 
     public ActionResult useOnBlock(ItemUsageContext context) {
