6e7bfe8aab90785af9e433d00c9b421c5294908e
shima <sh1m@outlook.jp>
2024-06-04 10:01:28 +0900

    Update version to v1.0.6.

--- common/src/main/java/com/shiri47s/mod/durabletools/blocks/DurableTorchBlock.java
+++ common/src/main/java/com/shiri47s/mod/durabletools/blocks/DurableTorchBlock.java
@@ -3,7 +3,6 @@ package com.shiri47s.mod.durabletools.blocks;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.TorchBlock;
 import net.minecraft.block.piston.PistonBehavior;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.BlockSoundGroup;
 
